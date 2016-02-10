98be169465ce4f87626cfb21737e65461315c0d2
cheyans <cheyan.set@gmail.com>
2016-02-10 13:14:20 -0500

    WHAT THE UFCK

--- batch-shell.c
+++ batch-shell.c
@@ -3,15 +3,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+
 int main() {
 	char *input;
 	char *program;
+	char *input_p;
 	int status;
 	while(input != "quit") {
-		printf("batch-shell>");
+		printf("batch-shell> ");
 		scanf("%s",input);
-		program = strtok(input, " ");
-		while(program != NULL) {
+		input_p = input;
+		printf("input: %s\n", input);
+		while(input_p != NULL) {
+			program = strsep(&input_p, " ");
+			//printf("%s", program);
 			int pid = fork();
 			if(pid == 0) {
 				execl("./", program, NULL);
@@ -19,7 +24,6 @@ int main() {
 			} else {
 				wait(&status);	
 			}
-			program = strtok(NULL, " ");
 		}
 	}
 }
