2daa26d00cd2ae3fc39369ed609154d2d4ccf07e
cheyans <cheyan.set@gmail.com>
2016-02-10 16:11:55 -0500

    malloc input

--- batch-shell.c
+++ batch-shell.c
@@ -5,25 +5,26 @@
 #include <string.h>
 
 int main() {
-	char *input;
+	char *input = (char *) malloc(sizeof(char));
 	char *program;
 	char *input_p;
 	int status;
-	while(input != "quit") {
+	while(1) {
 		printf("batch-shell> ");
 		scanf("%s",input);
+		if(strcmp(input, "quit") == 0){
+			exit(0);
+		}
 		input_p = input;
-		printf("input: %s\n", input);
 		while(input_p != NULL) {
 			program = strsep(&input_p, " ");
-			//printf("%s", program);
+			printf("%s\n", program);
 			int pid = fork();
 			if(pid == 0) {
 				execl("./", program, NULL);
 				exit(0);
-			} else {
-				wait(&status);	
 			}
+			waitpid(pid, &status, 0);
 		}
 	}
 }
