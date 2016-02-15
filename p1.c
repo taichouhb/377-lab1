cb2440dbd55d01f580a0d61690f2e1ef87358165
unknown <taichouhb@hotmail.com>
2016-02-15 13:38:00 -0500

    Added commits to P1 and P2, but P2 commits, may be incorect

--- p1.c
+++ p1.c
@@ -1,23 +1,34 @@
+
+//Imports 
 #include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+
 int main() {
+	//immediately fork
 	int c_pid = fork();
-	int g_pid;
+	int g_pid; //pointer for grandchild's process ID to be stored at
 	if(c_pid == 0) {
-		g_pid = fork();
+		//if we are in child process
+		g_pid = fork(); //fork into grandchild process
 		if(g_pid == 0) {
-			sleep(10);
+			//if we are in grandchild process
+			sleep(10); 
 			printf("Grandchild process with process id %d has completed.\n", getpid());
 		} else {
+			//when we are back in child process
 			int g_status;
+			//the child process needs to wait for the grandchild to finish executing
 			if(waitpid(g_pid, &g_status, 0)) {
 				printf("Child process with process ID %d has completed.\n", getpid());
 				exit(0);
 			}
 		}
 	} else {
+		//and now if we are not in child, but are in parent/root process, then this parent 
+		//simply waits for the child to finish executing before printing and finishing 
 		int c_status;
 		if(waitpid(c_pid, &c_status, 0)) {
 			printf("Parent process with process ID %d has finished.\n", getpid());
--- p2.c
+++ p2.c
@@ -1,15 +1,30 @@
+//imports
+
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
+
+
 int main() {
+
 	int id;
+	//Prompt for user or console input for a PID to kill
 	printf("Enter a pid: \n");
 	scanf("%d", &id);
-	if(getpgid(id) >= 0) {
+
+	//check to see if the id entered is a proper PID that is running on the system
+	if(getpgid(id) >= 0) {  
+		//check to see if the PID entered is within this processe's process group
+		//if it is, (kill returns a non zero number) then we have authority to kill 
 		if(kill(id,4) != 0) {
 			printf("You don't have permission to kill %d\n", id);
+			//need to return something here to indicate failure
 		}
+		//return something here to indicate success
+		//since we called kill in the if, kill is run. If an error turns up, then we'll catch
+		//it with the print, otherwise the process is killed without much indication. 
 	} else {
+		//if we get here that means 
 		printf("Process %d does not exist within killable group\n", id);
 	}
 }
